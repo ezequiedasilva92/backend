5df218222454f3cbb792b87e3ce77a3d0e8c103a
ezequiedasilva92 <ezequieldasilva92@hotmail.com>
2019-04-11 17:56:42 -0300

    alteração do file.js

--- src/models/File.js
+++ src/models/File.js
@@ -13,18 +13,18 @@ const File = new mongoose.Schema({
     
  }, 
  {
-    timestamps: true
+    timestamps: true,
     toObject: { virtuals: true },
     toJSON: { virtuals: true },
 
    }
 );
 
-File.virtual('url').get(function() {
-  const url = process.env.PORT || 'http://localhost:3333'
+File.virtual("url").get(function() {
+  const url = process.env.URL || 'http://localhost:3333'
 
    return `${url}/files/${encodeURIComponent(this.path)}`;
 
-})
+});
 
 module.exports = mongoose.model("File", File);
